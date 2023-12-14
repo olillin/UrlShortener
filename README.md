cd467705620f1c389ac856a27cfaf4d2837047a3
Oli <oli@hoodieboi.dev>
2023-12-14 22:10:02 +0100

    Add encryption functions

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ A webservice to generate and host shortened urls.
 
 This project uses [dotenv](https://www.npmjs.com/package/dotenv) to load environment variables and can be configured in the `.env` file.
 
-| Name        | Description                      | Default                                        |
-| ----------- | -------------------------------- | ---------------------------------------------- |
-| `PORT`      | Port the website will listen on. | `80` when using HTTP or `443` when using HTTPS |
-| `CERT_PATH` | File path to SSL certificate.    | `cert.pem`                                     |
-| `KEY_PATH`  | File path to SSL private key.    | `key.pem`                                      |
+| Name                | Description                         | Default                                        |
+| ------------------- | ----------------------------------- | ---------------------------------------------- |
+| `PORT`              | Port the website will listen on.    | `80` when using HTTP or `443` when using HTTPS |
+| `CERT_PATH`         | File path to SSL certificate.       | `cert.pem`                                     |
+| `KEY_PATH`          | File path to SSL private key.       | `key.pem`                                      |
+| `SECRET`            | Used as a global salt when hashing. | `secret`                                            |
+| `ENCRYPTION_IV`     | The [initialization vector](https://wikipedia.org/wiki/Initialization_vector) used for encryption. | ` ` |
+| `IDENTIFIER_LENGTH` | Length of the random identifier for shortened URLs. | `6`                            |
--- app.js
+++ app.js
@@ -1,10 +1,86 @@
 const express = require('express')
+const fs = require('fs')
+const crypto = require('crypto')
 
 require('dotenv/config')
+const SECRET = process.env.SECRET || ''
+const IDENTIFIER_LENGTH = parseInt(process.env.IDENTIFIER_LENGTH) || 6
+const encryptionIV = hash(process.env.ENCRYPTION_IV || '').substring(0, 16)
 
 const app = express()
 module.exports = app
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+const urls = {}
+
+app.use('/', express.static('./public/'))
+app.use(express.urlencoded({extended: true}))
+
+app.post('/api/signup/', (req, res) => {
+    const {email, password} = req.body
 })
+
+app.post('/api/create-url', (req, res) => {
+    const {url} = req.body
+
+    res.end(hash(url))
+})
+
+function hash(data) {
+    return crypto.createHash('sha512').update(data + SECRET).digest('base64')
+}
+
+function encrypt(data, key) {
+    const hashedKey = hashKey(key)
+    const cypher = crypto.createCipheriv('aes-256-cbc', hashedKey, encryptionIV)
+    return cypher.update(data, 'utf8', 'base64') + cypher.final('base64')
+}
+
+function decrypt(data, key) {
+    const hashedKey = hashKey(key)
+    const decypher = crypto.createDecipheriv('aes-256-cbc', hashedKey, encryptionIV)
+    return decypher.update(data, 'base64', 'utf8') + decypher.final('utf8')
+}
+
+function hashKey(key) {
+    return hash(key).substring(0, 32)
+}
+
+function generateIdentifer() {
+    const randomId = function() {
+        return crypto.randomBytes(IDENTIFIER_LENGTH)
+            .toString('base64')
+            .substring(0, IDENTIFIER_LENGTH)
+            .replace('+', '-')
+            .replace('/', '_')
+    }
+
+    const maxAttempts = 500
+    for (let i = 0; i < maxAttempts; i++) {
+        const id = randomId()
+        if (!getURL(id)) {
+            // URL is unused
+            return id
+        }
+    }
+    throw new Error(`Failed to generate identifier after ${maxAttempts} attempts`)
+}
+
+function getURL(id) {
+    const hashedId = hash(id)
+    const encryptedURL = urls[hashedId]
+    if (!encryptedURL) return undefined
+    const URL = decrypt(encryptedURL, id)
+    return URL
+}
+
+function saveURL(id, url) {
+    const entry = encryptURL(id, url)
+    const [key, value] = entry
+    urls[key] = value
+}
+
+function encryptURL(id, url) {
+    const hashedId = hash(id)
+    const encryptedURL = encrypt(url, id)
+    return [hashedId, encryptedURL]
+}
